8bc1cf4f9ade70833edfee435f334831842417a8
Rotem Tamir <rotemtamir@gmail.com>
2023-05-28 12:07:32 +0300

    dirs/kratos: initializing the kratos migration dir

---
+++ dirs/kratos/20221214101328000000_identity_delete_indices.mysql.sql
@@ -0,0 +1,7 @@
+CREATE INDEX identity_recovery_codes_identity_id_nid_idx ON identity_recovery_codes (identity_id, nid);
+CREATE INDEX identity_verification_codes_verifiable_address_nid_idx ON identity_verification_codes (identity_verifiable_address_id, nid);
+CREATE INDEX selfservice_settings_flows_identity_id_nid_idx ON selfservice_settings_flows (identity_id, nid);
+CREATE INDEX continuity_containers_identity_id_nid_idx ON continuity_containers (identity_id, nid);
+CREATE INDEX selfservice_recovery_flows_recovered_identity_id_nid_idx ON selfservice_recovery_flows (recovered_identity_id, nid);
+CREATE INDEX identity_recovery_tokens_identity_id_nid_idx ON identity_recovery_tokens (identity_id, nid);
+CREATE INDEX identity_recovery_codes_identity_recovery_address_id_nid_idx ON identity_recovery_codes (identity_recovery_address_id, nid);
---
+++ dirs/kratos/README.md
@@ -0,0 +1,9 @@
+### ory/kratos
+
+Migrations taken from [https://github.com/ory/kratos](ory/kratos).
+
+Automatically imported from `golang-migrate` using:
+
+```
+atlas migrate import
+```
