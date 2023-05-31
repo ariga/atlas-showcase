cc74b945a3c8fb63645d128534aad71626ca69b2
Rotem Tamir <rotemtamir@gmail.com>
2023-05-31 14:44:57 +0300

    dirs/ecommerce: comments seeds and inventory

--- dirs/ecommerce/schema.sql
+++ dirs/ecommerce/schema.sql
@@ -32,3 +32,39 @@ CREATE TABLE `product_reviews` (
   CONSTRAINT `product_reviews_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
 ) CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+-- Create "comments" table
+CREATE TABLE `comments` (
+  `id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `review_id` int,
+  `parent_comment_id` int,
+  `comment_text` text NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `user_id` (`user_id`),
+  INDEX `review_id` (`review_id`),
+  INDEX `parent_comment_id` (`parent_comment_id`),
+  CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`review_id`) REFERENCES `product_reviews` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `comments_ibfk_3` FOREIGN KEY (`parent_comment_id`) REFERENCES `comments` (`id`) ON DELETE CASCADE
+) CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Create "fulfillment_centers" table
+CREATE TABLE `fulfillment_centers` (
+  `id` int NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `location` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Create "inventory" table
+CREATE TABLE `inventory` (
+  `id` int NOT NULL,
+  `product_id` int NOT NULL,
+  `fulfillment_center_id` int NOT NULL,
+  `quantity` int NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `product_id` (`product_id`),
+  INDEX `fulfillment_center_id` (`fulfillment_center_id`),
+  CONSTRAINT `inventory_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `inventory_ibfk_2` FOREIGN KEY (`fulfillment_center_id`) REFERENCES `fulfillment_centers` (`id`) ON DELETE CASCADE
+) CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
