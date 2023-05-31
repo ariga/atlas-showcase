9603728f0c01be95ff170f1ef21c6018682a5426
Dat Dao <datdao@ariga.io>
2023-05-31 22:48:47 +0700

    dirs/ens: off pgaudit

--- dirs/exposure-notifications-server/000052_enable_pgaudit.down.sql
+++ dirs/exposure-notifications-server/000052_enable_pgaudit.down.sql
@@ -12,8 +12,8 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 
-BEGIN;
+-- BEGIN;
 
-DROP EXTENSION IF EXISTS pgaudit;
+-- DROP EXTENSION IF EXISTS pgaudit;
 
-END;
+-- END;
