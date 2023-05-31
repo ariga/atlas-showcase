b6182abd180718d9eb1a4c79bd7b825c242f01c3
Ronen Lubin <ronen7822@gmail.com>
2023-05-31 15:26:32 +0300

    add title to employees

---
+++ dirs/employees/migrations/20230531122555_titles.sql
@@ -0,0 +1,1 @@
+CREATE TABLE `titles` (`emp_no` int NOT NULL, `title` varchar(50) NOT NULL, `from_date` date NOT NULL, `to_date` date NULL, PRIMARY KEY (`emp_no`, `title`, `from_date`), CONSTRAINT `titles_ibfk_1` FOREIGN KEY (`emp_no`) REFERENCES `employees` (`emp_no`) ON UPDATE NO ACTION ON DELETE CASCADE) CHARSET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
