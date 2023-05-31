32e805c7d6fd48d06cac6efcfe3953eb04c90dd6
Giau. Tran Minh <giau.tmg@gmail.com>
2023-05-31 16:21:51 +0700

    coder: delete atlas migration format (#10)

--- dirs/coder/000038_template_max_ttl_cap_7_days.sql
+++
@@ -1,2 +0,0 @@
--- Set a cap of 7 days on template max_ttl
-UPDATE templates SET max_ttl = 604800000000000 WHERE max_ttl > 604800000000000;
--- dirs/coder/000105_rich_parameter_required.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE template_version_parameters ADD COLUMN required boolean NOT NULL DEFAULT true;
-COMMENT ON COLUMN template_version_parameters.required IS 'Is parameter required?';
