80f10d4919cde0bd80e1fc5911131603b08565b1
Dat. Ba Dao <datdao@ariga.io>
2023-05-31 23:31:40 +0700

    dirs/keto: postgres format (#34)

--- dirs/keto/0000000000000000000_inital.up.sql
+++ dirs/keto/0000000000000000000_inital.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE networks (
   id char(36) NOT NULL,
   PRIMARY KEY(id),
-  created_at DATETIME NOT NULL,
-  updated_at DATETIME NOT NULL
+  created_at TIMESTAMP(0) NOT NULL,
+  updated_at TIMESTAMP(0) NOT NULL
 );
