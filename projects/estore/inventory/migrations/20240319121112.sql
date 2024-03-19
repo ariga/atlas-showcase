e0cf6bd900fd039fccece574cbd4d0ef7476ebdd
Noam Cattan <62568565+noamcattan@users.noreply.github.com>
2024-03-19 14:40:44 +0200

    Update projects/estore/inventory/migrations/20240319121112.sql
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- projects/estore/inventory/migrations/20240319121112.sql
+++ projects/estore/inventory/migrations/20240319121112.sql
@@ -1,2 +1,9 @@
+-- atlas:txtar
+
+-- checks/destructive.sql --
+-- atlas:assert DS103
+SELECT NOT EXISTS (SELECT 1 FROM `products` WHERE `price` IS NOT NULL) AS `is_empty`;
+
+-- migration.sql --
 -- Modify "products" table
 ALTER TABLE `products` DROP COLUMN `price`;
