d31f539eb0f9693f8f0e52d8a6f2184f0aec983f
noamcattan <ndcattan@gmail.com>
2024-07-28 16:07:55 +0300

    rebase and disable lint

--- dirs/ecommerce/migrations/20240728130515.sql
+++ dirs/ecommerce/migrations/20240728130515.sql
@@ -1,4 +1,3 @@
--- Modify "comments" table
-ALTER TABLE `comments` DROP FOREIGN KEY `comments_ibfk_1`, DROP FOREIGN KEY `comments_ibfk_2`;
 -- Drop "comments" table
+-- atlas:nolint
 DROP TABLE `comments`;
