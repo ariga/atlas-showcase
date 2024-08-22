4ec6ae5da3149c17eddf020065330254ee369e87
GitHub Gen Changes <>
2024-08-22 13:09:11 +0000

    Renamed the column  to  in the  table, which is a backward-incompatible change.

--- dirs/ecommerce/schema.sql
+++ dirs/ecommerce/schema.sql
@@ -1,7 +1,7 @@
 -- Create 'users' table
 CREATE TABLE `users` (
 `id` int NOT NULL,
-`user_name` varchar(255) NOT NULL,
+`username` varchar(255) NOT NULL,
 `email` varchar(255) NOT NULL,
 `phone_number` varchar(15) NOT NULL,
 `is_admin` bool NULL DEFAULT 0,
@@ -14,7 +14,7 @@ CREATE TABLE `users` (
 `profile_picture_url` varchar(255) NULL,
 PRIMARY KEY (`id`),
 UNIQUE INDEX `email` (`email`),
-UNIQUE INDEX `user_name` (`user_name`),
+UNIQUE INDEX `username` (`username`),
 UNIQUE INDEX `phone_number` (`phone_number`)
 ) CHARSET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 
