204ede5bab8d6c614573d14d55e876c2ac2a7e0b
Noam Cattan <62568565+noamcattan@users.noreply.github.com>
2024-10-15 15:33:38 +0300

    Update dirs/ecommerce/migrations/20241001131216.sql
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- dirs/ecommerce/migrations/20241001131216.sql
+++ dirs/ecommerce/migrations/20241001131216.sql
@@ -1,2 +1,9 @@
+-- atlas:txtar
+
+-- checks/destructive.sql --
+-- atlas:assert DS103
+SELECT NOT EXISTS (SELECT 1 FROM `users` WHERE `user_name` IS NOT NULL) AS `is_empty`;
+
+-- migration.sql --
 -- Modify "users" table
 ALTER TABLE `users` DROP COLUMN `user_name`, ADD COLUMN `username` varchar(255) NOT NULL, ADD UNIQUE INDEX `username` (`username`);
