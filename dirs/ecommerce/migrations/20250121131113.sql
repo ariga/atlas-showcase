59654424d591e5c818329683224d40d9cc3e2a04
GitHub Gen Changes <>
2025-01-21 13:11:13 +0000

    Renamed the column 'email_address' to 'email' in the 'users' table to create a backward-incompatible change.

---
+++ dirs/ecommerce/migrations/20250121131113.sql
@@ -0,0 +1,2 @@
+-- Modify "users" table
+ALTER TABLE `users` DROP CHECK `users_chk_1`, ADD CONSTRAINT `users_chk_1` CHECK (regexp_like(`email`,_utf8mb4'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$')), DROP COLUMN `email_address`, ADD COLUMN `email` varchar(255) NOT NULL, DROP INDEX `user_name_email_address`, ADD UNIQUE INDEX `email` (`email`), ADD UNIQUE INDEX `user_name_email` (`user_name`, `email`);
