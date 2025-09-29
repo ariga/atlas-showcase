e986bb3cb199259eefea3a8a4b3e438ef809aead
GitHub Gen Changes <>
2025-09-29 10:06:00 +0000

    Added a NOT NULL constraint to the 'middle_name' column in the 'users' table to ensure that every user has a middle name recorded in the system.

--- dirs/ecommerce/schema.sql
+++ dirs/ecommerce/schema.sql
@@ -1,3 +1,4 @@
+
 -- Create 'users' table
 CREATE TABLE `users` (
     `id` int NOT NULL COMMENT 'Unique identifier for each user',
@@ -11,7 +12,7 @@ CREATE TABLE `users` (
     `updated_at` timestamp NULL ON UPDATE CURRENT_TIMESTAMP COMMENT 'Timestamp of the last update to the user record',
     `date_of_birth` date NOT NULL DEFAULT '1900-01-01' COMMENT 'Date of birth of the user',
     `gender` ENUM('male', 'female', 'other') NULL DEFAULT 'other' COMMENT 'User gender',
-    `middle_name` varchar(255) NULL COMMENT 'Middle name of the user, optional',
+    `middle_name` varchar(255) NOT NULL COMMENT 'Middle name of the user, now required',
     `active` bool NOT NULL DEFAULT 1 COMMENT 'Flag indicating if the user account is active, defaults to true',
     `last_login` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Timestamp of the last login of the user',
     `address` varchar(255) NOT NULL COMMENT 'Address of the user, now required',
