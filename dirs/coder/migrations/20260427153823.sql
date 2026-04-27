29c6ce6d8624e5db2a0b89d8538568017849f9e1
GitHub Gen Changes <>
2026-04-27 15:38:23 +0000

    [coder] Add a safe performance improvement by creating an index on workspace_builds(template_version_id) to speed up queries that list or aggregate builds by template version (common for reporting and template version rollouts). No tables added; only one new index.

---
+++ dirs/coder/migrations/20260427153823.sql
@@ -0,0 +1,2 @@
+-- Create index "workspace_builds_template_version_id_idx" to table: "workspace_builds"
+CREATE INDEX "workspace_builds_template_version_id_idx" ON "workspace_builds" ("template_version_id");
